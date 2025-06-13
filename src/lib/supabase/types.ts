abae5b1530efaee8d027ada4ee80f021c1fb4a27
Nat1anWasTaken <nathan@nat1an.xyz>
2025-06-13 12:27:17 +0800

    feat(*): adds label to pages table

--- src/lib/supabase/types.ts
+++ src/lib/supabase/types.ts
@@ -45,6 +45,7 @@ export type Database = {
           created_at: string;
           end_page: number | null;
           id: number;
+          label: string;
           start_page: number | null;
         };
         Insert: {
@@ -52,6 +53,7 @@ export type Database = {
           created_at?: string;
           end_page?: number | null;
           id?: number;
+          label: string;
           start_page?: number | null;
         };
         Update: {
@@ -59,6 +61,7 @@ export type Database = {
           created_at?: string;
           end_page?: number | null;
           id?: number;
+          label?: string;
           start_page?: number | null;
         };
         Relationships: [
